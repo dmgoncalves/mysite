06eda37cf3ce25787084b582f2bf5f1b058b0dcb
David Goncalves <dmg@secondexit.com>
2024-05-22 14:39:09 -0400

    new file:   .gcloudignore This was automatically created
    new file:   app.yaml
    This tells the server how to load the app
    
    modified:   flask_app.py
    Removed requests
    
    new file:   requirementsbaki.cfg

--- flask_app.py
+++ flask_app.py
@@ -1,12 +1,16 @@
 from flask import Flask
 
+
 app = Flask(__name__)
 
+def get_public_ip():
+    response = requests.get("https://api.ipify.org")
+    return response
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello, Aasdfasdfasdfin World!'
-
+    return 'hello'
 
 if __name__ == '__main__':
+
     app.run()
